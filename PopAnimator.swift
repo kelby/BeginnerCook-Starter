33f9ef6959f3031023aaee777b5e263ad4652447
Kelby Lee <leekelby@gmail.com>
2015-09-29 22:59:10 +0800

    conform to protocol, add transitionDuration & animateTransition methods.

---
+++ PopAnimator.swift
@@ -0,0 +1,19 @@
+//
+//  PopAnimator.swift
+//  BeginnerCook
+//
+//  Created by kelby on 15/9/29.
+//  Copyright © 2015年 Underplot ltd. All rights reserved.
+//
+
+import UIKit
+
+class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?)-> NSTimeInterval {
+        return 0
+    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        
+    }
+}
